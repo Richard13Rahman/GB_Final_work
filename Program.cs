323006ef3176700838c4641b46fd56c7edf2f90d
Rakhman <binataliev-a-g@yandex.ru>
2023-03-24 11:37:31 +0300

    Array input and output methods are introduced

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿string[] Entering_array(string[] str)
+{
+for (int i=0;i<str.Length;i++)
+{
+    System.Console.Write($"Введите {i+1} строкy массива:");
+    str[i]=Console.ReadLine();
+}
+return str;
+}
+
+string[] Output_array(string[] str)
+{
+for (int i=0;i<str.Length;i++)
+{
+    System.Console.Write($"{str[i]} ");
+}
+return str;
+}
+
+
+System.Console.Write("Введите сколько будет строк в массиве:");
+int size=Convert.ToInt32(Console.ReadLine());
+string[] str=new string[size];
+Entering_array(str);
+System.Console.WriteLine("Введенный строчный массив имеет вид:");
+Output_array(str);
+
+
+
+System.Console.WriteLine("Полученный массив имеет вид:");
